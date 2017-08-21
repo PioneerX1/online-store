90bf5e68273f17d779f21b1f0e32db9600062e67
Mick Sexton <lacrookedbeat@hotmail.com>
2017-08-21 11:14:02 -0700

    add about and marketplace components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,15 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { routing } from './app.routing';
+import { AboutComponent } from './about/about.component';
+import { MarketplaceComponent } from './marketplace/marketplace.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WelcomeComponent
+    WelcomeComponent,
+    AboutComponent,
+    MarketplaceComponent
   ],
   imports: [
     BrowserModule,
