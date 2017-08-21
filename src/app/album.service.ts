df5bb06474f3f01772b235bd75a9c482d749dd39
Mick Sexton <lacrookedbeat@hotmail.com>
2017-08-21 14:01:48 -0700

    add album service and register its provider in the marketplace component

---
+++ src/app/album.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { Album } from './album.model';
+import { ALBUMS } from './mock-albums';
+
+@Injectable()
+export class AlbumService {
+
+  constructor() { }
+
+  getAlbums() {
+    return ALBUMS;
+  }
+
+}
--- src/app/marketplace/marketplace.component.ts
+++ src/app/marketplace/marketplace.component.ts
@@ -1,26 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Album } from '../album.model';
+import { AlbumService } from '../album.service';
 
 
 @Component({
   selector: 'app-marketplace',
   templateUrl: './marketplace.component.html',
-  styleUrls: ['./marketplace.component.css']
+  styleUrls: ['./marketplace.component.css'],
+  providers: [AlbumService]
 })
 export class MarketplaceComponent implements OnInit {
-  constructor(private router: Router) { }
-  ngOnInit() { }
+
+  constructor(private router: Router, private albumService: AlbumService) { }
+
+  ngOnInit() {
+    this.albums = this.albumService.getAlbums();
+  }
 
   goToDetailPage(clickedAlbum: Album) {
     this.router.navigate(['albums', clickedAlbum.id]);
   }
 
-  albums: Album[] = [
-    new Album("Pulse", "Pink Floyd", "A live album by English progressive rock band released 1995.", 1),
-    new Album("Kid A", "Radiohead", "Alt-rock band's successful follow up to their OK Computer album, embracing electronica.", 2),
-    new Album("Oh Mercy", "Bob Dylan", "Dylan's 1987 comeback, teaming up with Grammy award winning producer, Daniel Lanois.", 3),
-    new Album("Time Out", "Dave Brubeck Quartet", "Cool Jazz album that experiments with time signatures, and features the alto saxophonist, Paul Desmond.", 4)
+  albums: Album[];
 
-  ];
 }
