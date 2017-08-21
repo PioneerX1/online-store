f32f5ceb7352a30a4b8e22f321f62b860170d13e
Mick Sexton <lacrookedbeat@hotmail.com>
2017-08-21 11:33:50 -0700

    add album class for model and display sample data for albums array on marketplace page

--- src/app/marketplace/marketplace.component.ts
+++ src/app/marketplace/marketplace.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Album } from '../album.model';
 
 @Component({
   selector: 'app-marketplace',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./marketplace.component.css']
 })
 export class MarketplaceComponent implements OnInit {
-
   constructor() { }
+  ngOnInit() { }
 
-  ngOnInit() {
-  }
+  albums: Album[] = [
+    new Album("Pulse", "Pink Floyd", "A live album by English progressive rock band released 1995.", 1),
+    new Album("Kid A", "Radiohead", "Alt-rock band's successful follow up to their OK Computer album, embracing electronica.", 2),
+    new Album("Oh Mercy", "Bob Dylan", "Dylan's 1987 comeback, teaming up with Grammy award winning producer, Daniel Lanois.", 3),
+    new Album("Time Out", "Dave Brubeck Quartet", "Cool Jazz album that experiments with time signatures, and features the alto saxophonist, Paul Desmond.", 4)
 
+  ];
 }
