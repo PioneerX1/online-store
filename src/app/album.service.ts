af1b894a329b50a7f0dee4b4ef863db3e3d5e2eb
Mick Sexton <lacrookedbeat@hotmail.com>
2017-08-22 15:23:21 -0700

    add save capability to firebase db, and output on marketplace component

--- src/app/album.service.ts
+++ src/app/album.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Album } from './album.model';
-import { ALBUMS } from './mock-albums';
+// import { ALBUMS } from './mock-albums';
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
 @Injectable()
@@ -16,12 +16,16 @@ export class AlbumService {
     return this.albums;
   }
 
+  addAlbum(newAlbum: Album) {
+    this.albums.push(newAlbum);
+  }
+
   getAlbumById(albumId: number) {
-    for(var i = 0; i < ALBUMS.length; i++) {
-      if(ALBUMS[i].id === albumId) {
-        return ALBUMS[i];
-      }
-    }
+    // for(var i = 0; i < ALBUMS.length; i++) {
+    //   if(ALBUMS[i].id === albumId) {
+    //     return ALBUMS[i];
+    //   }
+    // }
   }
 
 
